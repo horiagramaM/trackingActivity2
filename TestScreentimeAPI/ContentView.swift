0278aa11760bad928e3a1fda39f0bfbd05d48781
Horia Grama <hooria85@yahoo.com>
2024-07-08 19:54:02 +0300

    first commit please read README.md

---
+++ TestScreentimeAPI/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  TestScreentimeAPI
+//
+//  Created by Yazan Halawa on 7/27/21.
+//
+
+import SwiftUI
+import FamilyControls
+
+struct ContentView: View {
+    @StateObject var model = MyModel.shared
+    @State var isPresented = false
+    
+    var body: some View {
+        Button("Select Apps to Discourage") {
+            isPresented = true
+        }
+        .familyActivityPicker(isPresented: $isPresented, selection: $model.selectionToDiscourage)
+        Button("Start Monitoring") {
+            model.initiateMonitoring()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
